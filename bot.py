226436dd8d3be933739e86e788ffb7beb8281f91
tbohne <tbohne@uos.de>
2017-12-31 00:11:39 +0100

    tweet - speedtest results

--- bot.py
+++ bot.py
@@ -5,4 +5,12 @@ auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
 auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-api.update_status("tweet")
+import pyspeedtest
+st = pyspeedtest.SpeedTest()
+res = ""
+res += "PING: " + str(format(st.ping(), '.2f')) + " ms\n"
+res += "down: " + str(format(st.download() / 1000000.0, '.2f')) + "\n"
+res += "up: " + str(format(st.upload() / 1000000.0, '.2f'))
+print(res)
+
+api.update_status(res)
