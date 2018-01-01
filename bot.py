4c97c39808250502895e7b9b60b2e9a0f06d3159
tbohne <tbohne@uos.de>
2018-01-01 21:52:10 +0100

    changed speedtest lib

--- bot.py
+++ bot.py
@@ -1,16 +1,31 @@
 from secrets import *
 import tweepy
+import speedtest
 
-auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
-auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
-api = tweepy.API(auth)
+def format_speed(speed):
+    units = ['bps', 'Kbps', 'Mbps', 'Gbps']
+    unit = 0
+    while speed >= 1024:
+        speed /= 1024
+        unit += 1
+    return '%0.2f %s' % (speed, units[unit])
 
-import pyspeedtest
-st = pyspeedtest.SpeedTest()
-res = ""
-res += "PING: " + str(format(st.ping(), '.2f')) + " ms\n"
-res += "down: " + str(format(st.download() / 1000000.0, '.2f')) + "\n"
-res += "up: " + str(format(st.upload() / 1000000.0, '.2f'))
-print(res)
+if __name__ == '__main__':
+    auth = tweepy.OAuthHandler(C_KEY, C_SECRET)
+    auth.set_access_token(A_TOKEN, A_TOKEN_SECRET)
+    api = tweepy.API(auth)
 
-api.update_status(res)
+    servers = []
+    s = speedtest.Speedtest()
+    s.get_servers(servers)
+    s.get_best_server()
+    s.download()
+    s.upload()
+
+    results_dict = s.results.dict()
+    res = ""
+    res += "down: " + str(format_speed(results_dict['download'])) + "\n"
+    res += "up: " + str(format_speed(results_dict['upload'])) + "\n"
+    res += "ping: " + str(format(results_dict['ping'], '.2f')) + " ms"
+    print(res)
+    api.update_status(res)
