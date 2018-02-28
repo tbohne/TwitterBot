64831797463b8d8908041e00c931c02918607da2
tbohne <tbohne@uos.de>
2018-02-28 19:21:37 +0100

    bugfix - update movie for each tweet / post tweet once an hour

--- movie_bot.py
+++ movie_bot.py
@@ -1,8 +1,8 @@
-from app_data import *
 import tweepy
+import time
+from app_data import *
 from imdb import IMDb
 from random import randint
-import time
 
 if __name__ == '__main__':
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -10,16 +10,13 @@ if __name__ == '__main__':
     api = tweepy.API(auth)
 
     imdb = IMDb()
-
     top250 = imdb.get_top250_movies()
-    ranking_pos = randint(0, len(top250) - 1)
-    movie = top250[ranking_pos]
-
-    msg = 'Hey there, I have an interesting fact about {} for you!\n\n'.format(movie)
-
-    imdb.update(movie, 'trivia')
 
     while True:
+        ranking_pos = randint(0, len(top250) - 1)
+        movie = top250[ranking_pos]
+        msg = 'Hey there, I have an interesting fact about {} for you!\n\n'.format(movie)
+        imdb.update(movie, 'trivia')
         while True:
             trivia_idx = randint(0, len(movie['trivia']) - 1)
             fact = movie['trivia'][trivia_idx]
@@ -27,6 +24,6 @@ if __name__ == '__main__':
             if len(fact) <= 140:
                 break
         api.update_status(msg + fact)
-        # post a tweet every 10 minutes
-        print("sleeping for 10 minutes now..")
-        time.sleep(600)
+        # post a tweet every hour
+        print("sleeping for an hour now..")
+        time.sleep(3600)
