dfb9fdeb1aec38c23005cf6840c41a4e3f9bc19a
tbohne <tbohne@uos.de>
2018-02-28 18:16:08 +0100

    init movie bot with access to the imdb

---
+++ movie_bot.py
@@ -0,0 +1,10 @@
+from app_data import *
+import tweepy
+from imdb import IMDb
+
+if __name__ == '__main__':
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+
+    imdb = IMDb()
