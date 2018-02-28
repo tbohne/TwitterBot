8ea561b76ccb9c4e51481360064fa49f53134857
tbohne <tbohne@uos.de>
2018-02-28 18:49:22 +0100

    implemented basic functionality

--- movie_bot.py
+++ movie_bot.py
@@ -1,6 +1,7 @@
 from app_data import *
 import tweepy
 from imdb import IMDb
+from random import randint
 
 if __name__ == '__main__':
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -8,3 +9,20 @@ if __name__ == '__main__':
     api = tweepy.API(auth)
 
     imdb = IMDb()
+
+    top250 = imdb.get_top250_movies()
+    ranking_pos = randint(0, len(top250) - 1)
+    movie = top250[ranking_pos]
+
+    msg = 'Hey there, I have an interesting fact about {} for you!\n\n'.format(movie)
+
+    imdb.update(movie, 'trivia')
+
+    while True:
+        trivia_idx = randint(0, len(movie['trivia']) - 1)
+        fact = movie['trivia'][trivia_idx]
+        # tweets are only allowed to include <= 140 chars
+        if len(fact) <= 140:
+            break
+
+    api.update_status(msg + fact)
